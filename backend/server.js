9c266877a6cb7766ffb88120c1d5bbb04661efb0
r0hanr02 <rathodrohan721@gmail.com>
2025-12-03 19:57:49 +0530

    fix: configure CORS to allow specific origins and enable credentials

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ import aiRouter from "./routes/ai-routes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://synchat-green.vercel.app"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 connectDb();
 
